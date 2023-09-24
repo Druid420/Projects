8dd32e238fed0c9acf0ffd3adc1a53ae2493fbed
Druid420 <81491453+Druid420@users.noreply.github.com>
2023-09-23 23:22:38 -0400

    WebScrapper - Finds definitions and more for 1 wrd

---
+++ Projects/Google search/Def WebScrapper V1.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+#Ask for user input
+#print('Enter the Word(s) you would like the Definitons for.\nType "done" when you have entered all the words.')
+word = input('Enter the Word: ')
+
+#Get website's HTML code
+url = (f"https://www.google.com/search?q=define+{word}")
+html = requests.get(url)
+
+#Interpret HTML code
+soup = BeautifulSoup(html.content, 'html.parser')
+
+
+# Search for elements with class 'BNeawe s3v9rd AP7Wnd'
+results_def = soup.find_all('div', class_= 'BNeawe s3v9rd AP7Wnd')
+
+
+#Get 
+data = results_def[0].text
+split_data = data.split('.')
+p1 = data.split("\n")
+p2 = p1[1]
+p3_def = p2.split('.')
+p3_ex = p2.split('"')
+pos = p1[0]
+defin = p3_def[0]
+examp = p3_ex[1]
+
+#Print results
+print('\n\nWord:', word, '\nPart of Speech:', pos, '\nDefinition:', defin + '.', '\nExample:', examp + '.')
+
