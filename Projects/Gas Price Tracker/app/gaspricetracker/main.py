d677b6d0e6e70693eeb179d329fb2e84ed40d3c6
Druid420 <rishabhpatel1977@gmail.com>
2024-12-16 16:39:48 -0500

    Began implimentation of tinyDB

--- Projects/Gas Price Tracker/app/gaspricetracker/main.py
+++ Projects/Gas Price Tracker/app/gaspricetracker/main.py
@@ -1,34 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import json
 from collections import defaultdict
+from tinydb import TinyDB, Query
 
+db = TinyDB("db.json")
+zipcode = input("What zipcode? ")
+
+# Function to fetch and parse gas station data from the website
 def getData():
-  #get user zipcode and request fuel type
-  zipcode = input("What zipcode? ")
-  #fuel_type = input("What fuel type? ")
 
-  #url
-  url = (f"https://www.gasbuddy.com/home?search={zipcode}&fuel=1&maxAge=0&method=all")
+    # Construct the URL for fuel type 1 (regular) 
+    # TO DO: Update for ALL fuel types
+    url = (f"https://www.gasbuddy.com/home?search={zipcode}&fuel=1&maxAge=0&method=all")
 
-  # Fetch raw HTML content and turn it into parsable HTML using beautifulsoup
-  response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"})
-  soup = BeautifulSoup(response.content, 'html.parser')
+    # Fetch raw HTML content and turn it into parsable HTML using BeautifulSoup
+    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"})
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-  #find gas-station, price, time stamp, & addresses using tag then puts in list
-  gas_stations = [name.text for name in soup.findAll('a', href=re.compile("/station/"))]
-  prices = [price.text for price in soup.findAll('span', class_='text__xl___2MXGo text__left___1iOw3 StationDisplayPrice-module__price___3rARL')]
-  time_updated = [time.text for time in soup.findAll('span', class_ = 'ReportedBy-module__postedTime___J5H9Z')]
+    # Extract gas station names, prices, timestamps, and addresses
+    gas_stations = [name.text for name in soup.findAll('a', href=re.compile("/station/"))]
 
-  #gets addresses using findall then splits the div, removing the br/, and joins the two parts of the address
-  addresses = [" ".join(address_div.stripped_strings) for address_div in soup.findAll('div', class_ ='StationDisplay-module__address___2_c7v')]
+    prices = [price.text for price in soup.findAll('span', class_='text__xl___2MXGo text__left___1iOw3 StationDisplayPrice-module__price___3rARL')]
+    
+    time_updated = [time.text for time in soup.findAll('span', class_='ReportedBy-module__postedTime___J5H9Z')]
+    
+    addresses = [" ".join(address_div.stripped_strings) for address_div in soup.findAll('div', class_='StationDisplay-module__address___2_c7v')]
+    
+    # for station, price, time, address in zip(gas_stations, prices, time_updated, addresses):
+    #     db.insert({"store": station, "price": price, "time_updated": time, "address": address})
+    return zip(gas_stations, prices, time_updated, addresses)
 
-  #initalize a dictionary where the keys are the gas station names and key values are a list of dictionaries
-  store_data = defaultdict(list)
+def insertData():
+    #if the data base is empty then insert data
+    if not db.all():
+        for station, price, time, address in getData():
+            db.insert({"store": station, "price": price, "time_updated": time, "address": address})
+    else:
+        print("Can't do that! Something's in there!")
+def updateData():
+    pass
 
-  #populate dictionary
-  for station, price, time, address in zip(gas_stations, prices, time_updated, addresses):
-    store_data[station +" -- "+ address].append([price, time]) #keys are the gas station name PLUS the address for uniqueness
 
-  #return data
-  return store_data
+insertData()
+print(db.all())
+#db.drop_tables()
