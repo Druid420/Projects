e436cb0b459a7b8fb3ddd950b1cddb1d055a4978
Druid420 <81491453+Druid420@users.noreply.github.com>
2024-12-20 12:56:35 -0500

    Began working on sms feature

--- Projects/Gas Price Tracker/app/gaspricetracker/main.py
+++ Projects/Gas Price Tracker/app/gaspricetracker/main.py
@@ -1,12 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import json
-from collections import defaultdict
 from tinydb import TinyDB, Query
 
 db = TinyDB("db.json")
-zipcode = input("What zipcode? ")
+zipcode = 21502
+data = Query()
 
 # Function to fetch and parse gas station data from the website
 def getData():
@@ -24,25 +23,45 @@ def getData():
 
     prices = [price.text for price in soup.findAll('span', class_='text__xl___2MXGo text__left___1iOw3 StationDisplayPrice-module__price___3rARL')]
     
-    time_updated = [time.text for time in soup.findAll('span', class_='ReportedBy-module__postedTime___J5H9Z')]
+    #time_updated = [time.text for time in soup.findAll('span', class_='ReportedBy-module__postedTime___J5H9Z')]
     
     addresses = [" ".join(address_div.stripped_strings) for address_div in soup.findAll('div', class_='StationDisplay-module__address___2_c7v')]
     
-    # for station, price, time, address in zip(gas_stations, prices, time_updated, addresses):
-    #     db.insert({"store": station, "price": price, "time_updated": time, "address": address})
-    return zip(gas_stations, prices, time_updated, addresses)
+
+    print("Successfully retreived Data!")
+    return zip(gas_stations, prices, addresses)
 
 def insertData():
     #if the data base is empty then insert data
     if not db.all():
-        for station, price, time, address in getData():
-            db.insert({"store": station, "price": price, "time_updated": time, "address": address})
+        db.drop_tables()
+        for station, price, address in getData():
+            db.insert({"id": station+address, "name": station, "price": price, "address": address})
     else:
         print("Can't do that! Something's in there!")
+        
 def updateData():
-    pass
+    for station, latest_price, address in getData():
+      for store_info in db.search(data.id.exists()):
+          if store_info['id'] == station+address:
+              if latest_price != store_info['price']:
+                  db.update({'price': latest_price}, data.id == station+address)
+                  print(f'Price updated for {station}: {store_info["price"]} --> {latest_price}')
+              else:
+                  print(f'Price for {station} remained the same: {latest_price}')
 
+def sendMessage():
+    msg = EmailMessage()
+    msg.set_content('lets get a bag.')
 
-insertData()
-print(db.all())
-#db.drop_tables()
+    msg['From'] = senderEmail # 'email@address.com'
+    msg['To'] = gatewayAddress  # '1112223333@vmobl.com'
+    msg['Subject'] = 'Finance Family'
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(senderEmail, appKey)
+
+    server.send_message(msg)
+    server.quit()
+updateData()
